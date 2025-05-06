4beb17705c4574de9aed86819e1734247d891153
1990OKTexas <jamesmaddocks90@gmail.com>
2025-05-06 18:24:31 -0500

    Delete backend directory

--- backend/chat.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// TODO: Add chat routes
-
-module.exports = router;
--- backend/users.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// TODO: Add users routes
-
-module.exports = router;
